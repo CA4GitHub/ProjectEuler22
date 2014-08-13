f3c517c434f217644b1603e46934d2b3b66adf12
CA4GitHub <flawton@fredricsmacbook.att.net>
2014-08-13 14:28:57 -0700

    Added description of program and printed the score

--- ProjectEuler22.py
+++ ProjectEuler22.py
@@ -1,3 +1,18 @@
+'''This program solves the problem at https://projecteuler.net/problem=22.
+   (The names.txt file can be downloaded from the above url.)
+   The program reads in a file names.txt containing names enclosed in quotes and separated by commas.
+   e.g. "Jon","Jenny","Bob"  
+   It parses the file to produce a list of names.
+   It scores the file as follows:
+       1) Get the sum for each name. Each letter in a name adds a numeric value where A or a = 1 , B or b = 2, etc.
+       2) Multiply the sum of step 1 by the rank of the name in the sorted list
+       e.g If the first name is "Al", it's worth 1*(1 + 12) = 12.
+           If the second name is "Al", it's worth 2*(1 + 12) = 24.
+       3) Add the result of step 2 for all names in the list.
+   Finally, the program prints the score given to the file  
+'''
+
+
 def getNameScore(nameString):
     
     score = 0
@@ -43,5 +58,6 @@ def parseNamesFile(filePath):
 
 #main part of code
 filePath = 'names.txt'
-theTemp = parseNamesFile(filePath)
-print(theTemp)
+theNamesList = parseNamesFile(filePath)
+score = scoreListOfNames(theNamesList)
+print('The score of the file is {}.'.format(score))
